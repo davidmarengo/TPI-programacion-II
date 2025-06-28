c6faec9b70d3e12799c86b9474e0b4a859f6c95e
david marengo <electricistadavidmarengo@gmail.com>
2025-06-28 18:37:32 -0300

    menu principal

--- src/tpi/programacion/ii/controlador/Controlador.java
+++ src/tpi/programacion/ii/controlador/Controlador.java
@@ -1,6 +1,8 @@
 
 package tpi.programacion.ii.controlador;
 
+import tpi.programacion.ii.vista.Vista;
+
 
 
 public class Controlador {
@@ -8,17 +10,47 @@ public class Controlador {
     public Controlador() {
     }
     
-    
+    Vista vista = new Vista();
 
     public void iniciar(){
     
-    
+        int menu = 0;
         
+        do {
+            
+        
+          vista.mostrarMenu();
+         menu = vista.pedirNumero("Ingrese la oppcion que desea");
     
-    
-    
-    
-    
+        switch(menu){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 0:
+                break;
+            default:
+                break;
+                   
+        
+        }
+            
+        }while (menu !=0);
     
     
     
--- src/tpi/programacion/ii/vista/Vista.java
+++ src/tpi/programacion/ii/vista/Vista.java
@@ -13,7 +13,31 @@ public class Vista {
     
     public void mostrarMenu(){
     
-        System.out.println("");
+        //System.out.println("Seleccione la opcion que desea");
+        System.out.println("Opcion 1: Generar Campeonato");
+        System.out.println("Opcion 2: Generar Fecha");
+        System.out.println("Opcion 3: Generar Partido");
+        System.out.println("Opcion 4: Generar Equipo");
+        System.out.println("Opcion 5: Generar Jugador");
+        System.out.println("Opcion 6: Generar Director Tecnico");
+        System.out.println("Opcion 7: Generar Entrenador Fisico");
+        System.out.println("Opcion 8: Generar Medico");
+        System.out.println("Opcion 9: Generar Arbitro");
+        
     }
+    
+    public int pedirNumero(String mensaje){
+        System.out.println(mensaje);
+        return scanner.nextInt();
+    }
+    
+    
+    public String pedirDato(String mensaje){
+        System.out.println(mensaje);
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+    
+    
 
 }
