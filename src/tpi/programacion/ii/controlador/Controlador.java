e9d3a20c6f8015fa3f7962576223ef2779097255
david marengo <electricistadavidmarengo@gmail.com>
2025-06-28 20:57:30 -0300

    sub menu

--- src/tpi/programacion/ii/controlador/Controlador.java
+++ src/tpi/programacion/ii/controlador/Controlador.java
@@ -13,92 +13,131 @@ public class Controlador {
     Vista vista = new Vista();
 
     public void iniciar(){
-    
-        int menu = 0;
         
+    
+        //MENU PRINCIPAL
+        int opcionPrincipal;
+
         do {
-            
-        
-          vista.mostrarMenu();
-         menu = vista.pedirNumero("Ingrese la oppcion que desea");
-    
-        switch(menu){
-            case 1:
-                generarCampeoonato();
-                break;
-            case 2:
-                generarFecha();
-                break;
-            case 3:
-                generarPartido();
-                break;
-            case 4:
-                generarEquipo();
-                break;
-            case 5:
-                generarJugador();
-                break;
-            case 6:
-                generarDirectorTecnico();
-                break;
-            case 7:
-                generarEntrenadorFisico();
-                break;
-            case 8:
-                generarMedico();
-                break;
-            case 9:
-                generarArbitro();
-                break;
-            case 0:
-                vista.mostrarMensaje("Gracias");
-                break;
-            default:
-                break;
-                   
-        
-        }
-            
-        }while (menu !=0);
-    
-    
-    
-    
+            vista.mostrarMenuPrincipal();
+            opcionPrincipal = vista.pedirNumero("Seleccione una opción:");
+
+            switch (opcionPrincipal) {
+                case 1:
+                    menuMostrarInformacion();
+                    break;
+                case 2:
+                    menuCargarDatos();
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+            }
+        } while (opcionPrincipal != 0);
     }
 
+    // SUBMENU CARGAR DATOS
+    private void menuCargarDatos() {
+        int menu;
+        do {
+            vista.mostrarMenuDatos();
+            menu = vista.pedirNumero("Ingrese la opción que desea:");
+
+            switch (menu) {
+                case 1:
+                    generarCampeoonato();
+                    break;
+                case 2:
+                    generarFecha();
+                    break;
+                case 3:
+                    generarPartido();
+                    break;
+                case 4:
+                    generarEquipo();
+                    break;
+                case 5:
+                    generarJugador();
+                    break;
+                case 6:
+                    generarDirectorTecnico();
+                    break;
+                case 7:
+                    generarEntrenadorFisico();
+                    break;
+                case 8:
+                    generarMedico();
+                    break;
+                case 9:
+                    generarArbitro();
+                    break;
+                case 0:
+                    vista.mostrarMensaje("Volviendo al menú principal...");
+                    break;
+                default:
+                    vista.mostrarMensaje("Opción inválida. Intente nuevamente.");
+            }
+        } while (menu != 0);
+    }
+
+    // SUBMENU MOSTRAR INFORMACIÓN
+    private void menuMostrarInformacion() {
+        int opcion;
+        do {
+            vista.mostrarMenuInformacion();
+            opcion = vista.pedirNumero("Seleccione una opción:");
+
+            switch (opcion) {
+                case 1:
+                    mostrarCampeonato();
+                    break;
+                case 2:
+                    mostrarEquipos();
+                    break;
+                case 0:
+                    vista.mostrarMensaje("Volviendo al menú principal...");
+                    break;
+                default:
+                    vista.mostrarMensaje("Opción inválida.");
+            }
+        } while (opcion != 0);
+    }
+
+    // Métodos simulados para cargar datos
     private void generarCampeoonato() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     private void generarFecha() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     private void generarPartido() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-    private void generarArbitro() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private void generarMedico() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private void generarEntrenadorFisico() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private void generarDirectorTecnico() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private void generarJugador() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
+    
     private void generarEquipo() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-}
+    
+    private void generarJugador() {
+    }
+    
+    private void generarDirectorTecnico() {
+    }
+    
+    private void generarEntrenadorFisico() {
+    }
+    
+    private void generarMedico() {
+    }
+    
+    private void generarArbitro() {
+    }
+
+    private void mostrarCampeonato() {
+    }
+
+    private void mostrarEquipos() {
+    }
+
+   }
+
--- src/tpi/programacion/ii/vista/Vista.java
+++ src/tpi/programacion/ii/vista/Vista.java
@@ -11,21 +11,7 @@ public class Vista {
     public Vista() {
     }
     
-    public void mostrarMenu(){
-    
-        //System.out.println("Seleccione la opcion que desea");
-        System.out.println("Opcion 1: Generar Campeonato");
-        System.out.println("Opcion 2: Generar Fecha");
-        System.out.println("Opcion 3: Generar Partido");
-        System.out.println("Opcion 4: Generar Equipo");
-        System.out.println("Opcion 5: Generar Jugador");
-        System.out.println("Opcion 6: Generar Director Tecnico");
-        System.out.println("Opcion 7: Generar Entrenador Fisico");
-        System.out.println("Opcion 8: Generar Medico");
-        System.out.println("Opcion 9: Generar Arbitro");
         
-    }
-    
     public int pedirNumero(String mensaje){
         System.out.println(mensaje);
         return scanner.nextInt();
@@ -40,6 +26,31 @@ public class Vista {
     
     public void mostrarMensaje(String mensaje){
         System.out.println(mensaje);
+    }
+
+    public void mostrarMenuPrincipal() {
+        System.out.println("Opcion 1:Ingresar al menu para cargar datos");
+        System.out.println("Opcion 2:Ingresar al menu para mostrar informacion");
+        System.out.println("Opcion 0:Salir del sistema");
+        
+    }
+
+    public void mostrarMenuInformacion() {
+         System.out.println("Opcion 1: Generar Campeonato");
+        System.out.println("Opcion 2: Generar Fecha");
+        System.out.println("Opcion 3: Generar Partido");
+        System.out.println("Opcion 4: Generar Equipo");
+        System.out.println("Opcion 5: Generar Jugador");
+        System.out.println("Opcion 6: Generar Director Tecnico");
+        System.out.println("Opcion 7: Generar Entrenador Fisico");
+        System.out.println("Opcion 8: Generar Medico");
+        System.out.println("Opcion 9: Generar Arbitro");
+        System.out.println("Opcion 0: Volver al menu principal");
+    }
+
+    public void mostrarMenuDatos() {
+        System.out.println("Opcion 1: Mostrar informacion del campeonato");
+        System.out.println("Opcion 2: Mostrar informacion de equipos");
     }
 
 }
